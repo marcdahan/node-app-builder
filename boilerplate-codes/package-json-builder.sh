13a2d9ab4ffaea1a4bce593f7fe7847d91f983ba
Marc Dahan <marcdahan@gmail.com>
2021-05-04 06:04:27 +0200

    minor modifications

--- boilerplate-codes/package-json-builder.sh
+++ boilerplate-codes/package-json-builder.sh
@@ -9,13 +9,12 @@ cat << _EOF_ >> $1/package.json
     "keywords": [],
     "license": "MIT",
     "devDependencies": {
-        /*express*/
         "body-parser": "1.19.0",
         "express": "4.17.1",
-        /*gulp + sass + browserSync*/
         "browser-sync": "^2.26.13",
         "cssnano": "^4.1.10",
         "gulp": "^4.0.2",
+        "gulp-nodemon": "^2.5.0",
         "gulp-postcss": "^9.0.0",
         "gulp-sass": "^4.1.0",
         "gulp-terser": "^1.4.0",
